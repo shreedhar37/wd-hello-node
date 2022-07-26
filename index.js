61a66f1f3a4febb2f2891a2aaf267d325a869ee5
Shreedhar Chavan <meshree4@gmail.com>
2022-07-26 14:57:26 +0530

    Fs module added

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-function hello() {
-  console.log("Hello Node.Js! Trying it for the first time");
-}
+// load fs (file system) module
+const fs = require("fs");
 
-hello();
-console.log("Hello GitHub!!!");
+// write into the file  sample.txt
+
+fs.writeFile(
+  "sample.txt",
+  "Hello World. Welcome to Node.js File System module.",
+  function (err) {
+    if (err) throw err;
+    console.log("File created!!!");
+  }
+);
+
+// read from the file sample.txt
+
+fs.readFile("sample.txt", function (err, data) {
+  if (err) throw err;
+  console.log(data.toString());
+});
+
+// append the contents in file sample.txt
+
+fs.appendFile("sample.txt", "This is my updated content", function (err) {
+  if (err) throw err;
+  console.log("File is updated!!");
+});
+
+// rename the file 'sample.txt.'
+fs.rename("sample.txt", "test.txt", function (err) {
+  if (err) throw err;
+  console.log("File name updated !!!");
+});
+
+// delete the file in ecosystem
+fs.unlink("test.txt", function (err) {
+  if (err) throw err;
+  console.log("File test.txt deleted successfully!!");
+});
