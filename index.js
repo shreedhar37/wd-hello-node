c25090a99bb53ea5a4ee285702e913d99d2740d9
Shreedhar Chavan <meshree4@gmail.com>
2022-07-28 12:58:26 +0530

    readLine added

--- index.js
+++ index.js
@@ -75,40 +75,79 @@
 //       response.end();
 //     })
 //     .listen(3000);
+//console.log("Server is running on localhost:3000");
+//console.log("Press ctrl + c to stop the server");
+
 // });
 
 // use path to display HTML
 
-const http = require("http");
+// const http = require("http");
+// const fs = require("fs");
+
+// let homeContent;
+// let projectContent;
+
+// fs.readFile("home.html", function (err, home) {
+//   if (err) throw err;
+//   homeContent = home;
+// });
+
+// fs.readFile("project.html", function (err, project) {
+//   if (err) throw err;
+//   projectContent = project;
+// });
+
+// http
+//   .createServer(function (request, response) {
+//     let url = request.url;
+//     response.writeHeader(200, { "Content-Type": "text/html" });
+//     switch (url) {
+//       case "/project":
+//         response.write(projectContent);
+//         response.end();
+//         break;
+
+//       default:
+//         response.write(homeContent);
+//         response.end();
+//         break;
+//     }
+//   })
+//   .listen(3000);
+// console.log("Server is running on localhost:3000");
+//console.log("Press ctrl + c to stop the server");
+
+// ReadLine library
+// const readLine = require("readline");
+
+// const lineDetail = readLine.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// lineDetail.question(`Please provide your name : `, (name) => {
+//   console.log(`Hi ${name}`);
+//   lineDetail.close();
+// });
+
+// display HTML by taking file path from user
+const readLine = require("readline");
 const fs = require("fs");
+const http = require("http");
 
-let homeContent;
-let projectContent;
-
-fs.readFile("home.html", function (err, home) {
-  if (err) throw err;
-  homeContent = home;
+const lineDetail = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-fs.readFile("project.html", function (err, project) {
-  if (err) throw err;
-  projectContent = project;
+lineDetail.question(`Please provide a path to the file : `, (path) => {
+  const server = http.createServer(function (request, response) {
+    const stream = fs.createReadStream(`${path}`);
+    stream.pipe(response);
+  });
+  lineDetail.close();
+  server.listen(3000);
+  console.log("Server is running on localhost:3000");
+  console.log("Press ctrl + c to stop the server");
 });
-
-http
-  .createServer(function (request, response) {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
-    }
-  })
-  .listen(3000);
